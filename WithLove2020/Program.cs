2927fd2e9cc1815168dc12230acb42721f4df222
marcio henrique <mhmuzy@embratel.com.br>
2020-05-12 15:32:54 -0300

    Adicionar arquivos de projeto.

---
+++ WithLove2020/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WithLove2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
